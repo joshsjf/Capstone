53dcc654991e5dc9c128bef1897b46865e3581eb
Judah <judahmcnicholl@gmail.com>
2018-08-29 14:08:05 +1200

    File restructured, mysql added in settings.py

--- Projects/techpalmy/techpalmyapp/settings.py
+++ Projects/techpalmy/techpalmyapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sites'
+    'sites',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'techpalmyapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'techpalmy',
+        'USER': 'root',
+        'PASSWORD': 'Capstoners!123'
     }
 }
 
