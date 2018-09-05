ec266e8d1961c402f048b0058594ffc4f6615a61
jjmassey <joshua.fleet.1@uni.massey.ac.nz>
2018-09-05 12:13:15 +1200

    timezone setting update - working

--- Projects/techpalmy/techpalmyapp/techpalmyapp/settings.py
+++ Projects/techpalmy/techpalmyapp/techpalmyapp/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'Pacific/Auckland'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Pacific/Auckland'
 
 USE_I18N = True
 
