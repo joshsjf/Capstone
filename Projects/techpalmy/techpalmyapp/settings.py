e181bddb6f89cd3d67c1af09c371058facf7c20e
Judah <judahmcnicholl@gmail.com>
2018-09-19 13:33:28 +1200

    Can create users now.

--- Projects/techpalmy/techpalmyapp/settings.py
+++ Projects/techpalmy/techpalmyapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'sites.apps.SitesConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- Projects/techpalmy/users/views.py
+++ Projects/techpalmy/users/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from users.forms import UserRegisterForm
+
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, "Account created for {}!".format(username))
             return redirect('sites-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 
