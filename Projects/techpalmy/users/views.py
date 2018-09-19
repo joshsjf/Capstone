652c35b1d844a62e8d5d934b3a68105367177be2
Judah <judahmcnicholl@gmail.com>
2018-09-19 13:17:05 +1200

    Can create a user now.

--- Projects/techpalmy/users/views.py
+++ Projects/techpalmy/users/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, "Account created for {}!".format(username))
+            return redirect('sites-home')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
-    
+
+
+# messages.debug
+# messages.info
+# messages.success
+# messages.warning
+# messages.error
