569902cf650be328fbfdac405086d5cbefc778b6
Judah <judahmcnicholl@gmail.com>
2018-09-19 20:17:52 +1200

    Email reset has been set up.

--- Projects/techpalmy/techpalmyapp/settings.py
+++ Projects/techpalmy/techpalmyapp/settings.py
@@ -131,3 +131,10 @@ LOGIN_URL = 'login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'techpalmyemailer@gmail.com'
+EMAIL_HOST_PASSWORD = 'Capstoners!123'
