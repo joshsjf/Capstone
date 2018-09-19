82af57187f1e779c54c6175bdccd9214b6ed4cc2
Judah <judahmcnicholl@gmail.com>
2018-09-19 15:19:17 +1200

    User and profile picture completed

--- Projects/techpalmy/techpalmyapp/settings.py
+++ Projects/techpalmy/techpalmyapp/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'sites-home'
+
 LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- Projects/techpalmy/techpalmyapp/urls.py
+++ Projects/techpalmy/techpalmyapp/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,6 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
