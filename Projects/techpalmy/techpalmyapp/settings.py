a71722b2c7f76c45ea2fcf63302f2fe55edf5f34
Judah <judahmcnicholl@gmail.com>
2018-09-19 12:24:51 +1200

    Django convention to have template/appname Added in the users app

--- Projects/techpalmy/techpalmyapp/settings.py
+++ Projects/techpalmy/techpalmyapp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sites',
+    'sites.apps.SitesConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
