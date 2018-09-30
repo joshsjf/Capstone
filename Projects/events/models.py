eb9a6769487f16a57b202aeba711e59b7de5d0fd
Glen Ward <arcangw@gmail.com>
2018-10-01 00:16:47 +1300

    added the base of models

--- Projects/events/models.py
+++ Projects/events/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from users.models import Profile
 
-# Create your models here.
+class EventListing(models.Model):
+    def __str__(self):
+        return self.title
