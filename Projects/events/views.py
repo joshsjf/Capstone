e8e11005e789483220edc603c017683154fa56fe
Glen Ward <arcangw@gmail.com>
2018-10-01 00:18:48 +1300

    Added to the views

--- Projects/events/views.py
+++ Projects/events/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import EventListing
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+class EventCreateView(LoginRequiredMixin, CreateView):
+	model = EventListing
+	fields = [] #models go here
+
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+class EventPageView(ListView):
+	model = EventListing
+	template_name = 'companies/company.html'
+	context_object_name = 'data'
+	ordering = ['-date_posted']
+
+class EventCompanyPageView(ListView):
+	model = EventListing
+	template_name = 'companies/user_company.html'
+	context_object_name = 'data'
+	paginate_by = 3
+
+	def get_queryset(self):
+		user = get_object_or_404(User, username=self.kwargs.get('username'))
+		return CompanyListing.objects.filter(author=user).order_by('-date_posted')
+
+class EventUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = EventListing
+	fields = [] #models go here
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		company = self.get_object()
+		return self.request.user == company.author		# do we need a conditional true/false here??
+
+
+class EventDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = EventListing
+	success_url = '/'
+	def test_func(self):
+		company = self.get_object()
+		return self.request.user == company.author
+
+
+class EventDetailView(DetailView):
+	model = EventListing
