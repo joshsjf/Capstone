f05cf9d14aa0ec24cc0d8a3d9f867254dbbf7b88
Judah <judahmcnicholl@gmail.com>
2018-10-01 16:06:42 +1300

    didn't merge over a field

--- Projects/companies/models.py
+++ Projects/companies/models.py
@@ -14,6 +14,8 @@ class CompanyListing(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(default = timezone.now)
 
+    image = models.ImageField(default='default.jpg', upload_to='company_pics')
+    
     companyName = models.CharField(max_length = 20)
     contactName = models.CharField(max_length = 20)
     email = models.EmailField()
