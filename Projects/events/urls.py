ecc6963d0f73c3bba4d15a980f64937f743b4527
Glen Ward <arcangw@gmail.com>
2018-10-02 16:13:44 +1300

    Added two URL paths

--- Projects/events/urls.py
+++ Projects/events/urls.py
@@ -4,5 +4,6 @@ from . import views as event_views
 
 urlpatterns = [
     path('', EventPageView.as_view(), name='sites-events'),
-    path('<int:pk>/', EventDetailView.as_view(), name='event-detail')
+    path('<int:pk>/', EventDetailView.as_view(), name='event-detail'),
+    path('new/', event_views.eventCreate, name='event-create')
 ]
