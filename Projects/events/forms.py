339f8cc0b93f2e9850b08da8abe53a96c2910f57
Glen Ward <arcangw@gmail.com>
2018-10-06 18:40:37 +1300

    Updated fields

--- Projects/events/forms.py
+++ Projects/events/forms.py
@@ -5,4 +5,4 @@ from events.models import EventListing
 class EventCreateForm(forms.ModelForm):
     class Meta:
         model = JobListing
-        fields = ['author', 'date_posted', 'eventWhen', 'eventWhere', 'eventDescription', 'registrationCost', 'eventWebsite']
+        fields = ['author', 'date_posted', 'eventName', 'eventWhen', 'eventLocation', 'eventDescription', 'registrationCost', 'eventWebsite']
