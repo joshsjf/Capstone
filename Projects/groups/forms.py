ff6802faaab9394511cc48c4ac92cf303e4606a0
jjmassey <joshua.fleet.1@uni.massey.ac.nz>
2018-10-08 12:38:24 +1300

    models groups

--- Projects/groups/forms.py
+++ Projects/groups/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 from .models import ConsultantListing
 
-class ConsultantCreateView(forms.ModelForm):
-    tscs = forms.BooleanField(required=True)
+class GroupCreateView(forms.ModelForm):
+    termsAndConditions = forms.BooleanField(required=True)
 
     class Meta:
-        model = ConsultantListing
-        fields = ['groupName', 'image', 'description', 'term']
+        model = GroupListing
+        fields = ['groupName', 'image', 'description', 'termsAndConditions']
 
-class ConsultantUpdateForm(forms.ModelForm):
+class GroupUpdateForm(forms.ModelForm):
     pk = ''
     id = ''
     class Meta:
-        model = ConsultantListing
-        fields = ['groupName', 'image', 'description', 'tscs']
+        model = GroupListing
+        fields = ['groupName', 'image', 'description', 'termsAndConditions']
--- Projects/groups/models.py
+++ Projects/groups/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django import forms
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from PIL import Image
 
-# Create your models here.
+
+class GroupListing(models.Model):
+    def __str__(self):
+        return self.groupName
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default = timezone.now)
+
+    groupName = models.CharField(max_length = 20)
+    image = models.ImageField(default='default.jpg', upload_to='consultant_pics')
+
+    description = models.TextField()
+    termsAndConditions = models.BooleanField()
+
+    def get_absolute_url(self):
+        return reverse('cons-detail', kwargs={'pk': self.pk})
+
+    def save(self, **kwargs):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
