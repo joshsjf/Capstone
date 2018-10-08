0b33842aa64c123c6e612b9de674f105233e9aa0
Glen Ward <arcangw@gmail.com>
2018-10-08 16:17:31 +1300

    Added URL patterns

--- Projects/events/urls.py
+++ Projects/events/urls.py
@@ -5,5 +5,8 @@ from . import views as event_views
 urlpatterns = [
     path('', EventPageView.as_view(), name='sites-events'),
     path('<int:pk>/', EventDetailView.as_view(), name='event-detail'),
-    path('new/', event_views.eventCreate, name='event-create')
+    path('new/', event_views.eventCreate, name='event-create'),
+    path('<str:username>/events', UserEventPageView.as_view(), name='user-event'),
+    path('<int:pk>/update/',event_views.EventUpdateView, name='event-update'),
+    path('<int:pk>/delete/', EventDeleteView.as_view(), name='event-delete')
 ]
