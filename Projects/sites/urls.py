41b5d968b368d1fcd243a2f6bfad485f4d1e8079
Judah <judahmcnicholl@gmail.com>
2018-10-08 17:27:14 +1300

    searching done, and changed false to False

--- Projects/sites/urls.py
+++ Projects/sites/urls.py
@@ -5,7 +5,7 @@ from sites import views
 
 urlpatterns = [
     path('', views.home, name='sites-home'),
-
+    path('search/', views.search, name='search'),
     path('AboutUs/', views.AboutPageView.as_view(), name='sites-about'),
     path('Contact/', views.ContactPageView.as_view(), name='sites-contact'),
     path('Education/', views.EducationPageView.as_view(), name='sites-education'),
