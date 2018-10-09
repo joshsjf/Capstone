5c16face064616889b9340390235a877fca6f2f7
Glen Ward <arcangw@gmail.com>
2018-10-09 13:18:42 +1300

    Edited char_lengths, removed author.

--- Projects/events/forms.py
+++ Projects/events/forms.py
@@ -5,15 +5,15 @@ class EventCreateView(forms.ModelForm):
     terms_And_Conditions = forms.BooleanField(required=True)
     class Meta:
         model = EventListing
-        fields = ['author', 'date_posted', 'event_Name', 'event_When', 'event_Location', 'event_Description', 'registration_Cost', 'event_Website']
+        fields = ['date_posted', 'event_Name', 'event_When', 'event_Location', 'event_Description', 'registration_Cost', 'event_Website']
 
 
 class EventCreateForm(forms.ModelForm):
     class Meta:
         model = EventListing
-        fields = ['author', 'date_posted', 'event_Name', 'event_When', 'event_Location', 'event_Description', 'registration_Cost', 'event_Website']
+        fields = ['date_posted', 'event_Name', 'event_When', 'event_Location', 'event_Description', 'registration_Cost', 'event_Website']
 
 class EventUpdateForm(forms.ModelForm):
     class Meta:
         model = EventListing
-        fields = ['author', 'date_posted', 'event_Name', 'event_When', 'event_Location', 'event_Description', 'registration_Cost', 'event_Website']
+        fields = ['date_posted', 'event_Name', 'event_When', 'event_Location', 'event_Description', 'registration_Cost', 'event_Website']
