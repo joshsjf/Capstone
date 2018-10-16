ce53a4942bdf2e091f807941e189029f3745e284
jjmassey <joshua.fleet.1@uni.massey.ac.nz>
2018-10-16 17:45:48 +1300

    T's and C's must be ticked when UPDATED

--- Projects/companies/forms.py
+++ Projects/companies/forms.py
@@ -10,6 +10,7 @@ class CompanyCreateView(forms.ModelForm):
     				'industry', 'specialist_Area', 'type_Of_Business', 'receive_newsletter', 'description', 'terms_And_Conditions']
 
 class CompanyUpdateForm(forms.ModelForm):
+    terms_And_Conditions = forms.BooleanField(required=True, label = "<a href='/termsandconditions' target='_blank'>Terms and Conditions</a>")
     class Meta:
         model = CompanyListing
         fields = ['company_Name', 'image', 'contact_Name', 'email', 'phone_Number', 'website', 'number_Of_Employees',
--- Projects/events/forms.py
+++ Projects/events/forms.py
@@ -9,11 +9,13 @@ class EventCreateView(forms.ModelForm):
 
 
 class EventCreateForm(forms.ModelForm):
+    terms_And_Conditions = forms.BooleanField(required=True, label = "<a href='/termsandconditions' target='_blank'>Terms and Conditions</a>")
     class Meta:
         model = EventListing
         fields = ['event_Name', 'event_Time_dd_mm_yyyy', 'event_Location', 'event_Description', 'registration_Cost', 'event_Website']
 
 class EventUpdateForm(forms.ModelForm):
+    terms_And_Conditions = forms.BooleanField(required=True, label = "<a href='/termsandconditions' target='_blank'>Terms and Conditions</a>")
     class Meta:
         model = EventListing
         fields = ['event_Name', 'event_Time_dd_mm_yyyy', 'event_Location', 'event_Description', 'registration_Cost', 'event_Website']
