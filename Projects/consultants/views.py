dc73a1a836a81b4ae46548c40d4ffc7b35de727a
Judah <judahmcnicholl@gmail.com>
2018-10-18 11:47:09 +1300

    fixed updating images on the 3 places.

--- Projects/consultants/views.py
+++ Projects/consultants/views.py
@@ -24,7 +24,7 @@ def consultantCreate(request, **kwargs):
 
 def consultantUpdateView(request, pk):
 	instance = get_object_or_404(ConsultantListing, id=pk)
-	form = ConsultantUpdateForm(request.POST or None, instance=instance)
+	form = ConsultantUpdateForm(request.POST or None, request.FILES, instance=instance)
 	if form.is_valid():
 		form.save()
 		messages.success(request, "Consultant profile has been updated!")
