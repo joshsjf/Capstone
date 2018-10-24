43b71328898baf8ff46a638d4ce0fcdd6e519aa9
jjmassey <joshua.fleet.1@uni.massey.ac.nz>
2018-10-25 10:40:00 +1300

    update companies model

--- Projects/companies/forms.py
+++ Projects/companies/forms.py
@@ -14,7 +14,9 @@ class CompanyCreateView(forms.ModelForm):
     class Meta:
         model = CompanyListing
         fields = ['company_Name', 'lat', 'lon', 'image', 'contact_Name', 'email', 'phone_Number', 'website', 'number_Of_Employees',
-    				'industry', 'specialist_Area', 'type_Of_Business', 'description', 'takes_On_Summer_Students', 'terms_And_Conditions']
+    				'industry', 'specialist_Area', 'description', 'takes_On_Summer_Students', 'terms_And_Conditions']
+
+# 'type_Of_Business'
 
 class CompanyUpdateForm(forms.ModelForm):
     terms_And_Conditions = forms.BooleanField(required=True, label = "<a href='/termsandconditions' target='_blank'>Terms and Conditions</a>")
@@ -29,4 +31,6 @@ class CompanyUpdateForm(forms.ModelForm):
     class Meta:
         model = CompanyListing
         fields = ['company_Name', 'lat', 'lon', 'image', 'contact_Name', 'email', 'phone_Number', 'website', 'number_Of_Employees',
-    				'industry', 'specialist_Area', 'type_Of_Business', 'description', 'takes_On_Summer_Students', 'terms_And_Conditions']
+    				'industry', 'specialist_Area', 'description', 'takes_On_Summer_Students', 'terms_And_Conditions']
+
+# 'type_Of_Business'
