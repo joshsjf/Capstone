1ad9c678b51275e2bd41d03f52f87ce4c2c8e5db
jjmassey <joshua.fleet.1@uni.massey.ac.nz>
2018-10-25 10:58:42 +1300

    companies

--- Projects/companies/models.py
+++ Projects/companies/models.py
@@ -43,7 +43,7 @@ class CompanyListing(models.Model):
 
         img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+        if img.height > 500 or img.width > 500:
+            output_size = (500, 500)
             img.thumbnail(output_size)
             img.save(self.image.path)
