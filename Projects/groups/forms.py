a40aaea1f437f961bbeea9f915531406602d036d
Glen Ward <arcangw@gmail.com>
2018-10-24 14:41:25 +1300

    Made changes to groups

--- Projects/groups/forms.py
+++ Projects/groups/forms.py
@@ -6,10 +6,10 @@ class GroupCreateView(forms.ModelForm):
 
     class Meta:
         model = GroupListing
-        fields = ['group_Name', 'image', 'date', 'location', 'description', 'terms_And_Conditions']
+        fields = ['group_Name', 'image', 'location', 'description', 'terms_And_Conditions']
 
 class GroupUpdateForm(forms.ModelForm):
     terms_And_Conditions = forms.BooleanField(required=True, label = "<a href='/termsandconditions' target='_blank'>Terms and Conditions</a>")
     class Meta:
         model = GroupListing
-        fields = ['group_Name', 'image', 'date', 'location', 'description', 'terms_And_Conditions']
+        fields = ['group_Name', 'image', 'location', 'description', 'terms_And_Conditions']
